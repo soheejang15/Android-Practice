65120ad3804fad61b9af80b1239f990c4d044d53
soheejang15 <jsh167528@gmail.com>
2018-07-06 12:06:57 +0900

    Condition 1 and 2 completed
    i) Display a message of toast
    ii) Activity movement

--- ViewPagerPractice/app/src/main/java/com/example/dsm2018/viewpagerpractice/LoginActivity.java
+++ ViewPagerPractice/app/src/main/java/com/example/dsm2018/viewpagerpractice/LoginActivity.java
@@ -1,9 +1,14 @@
 package com.example.dsm2018.viewpagerpractice;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -18,5 +23,23 @@ public class LoginActivity extends AppCompatActivity {
         loginLoginButton = (Button) findViewById(R.id.button_login_login);
         loginIdEdit = (EditText) findViewById(R.id.edit_login_id);
         loginPwEdit = (EditText) findViewById(R.id.edit_login_pw);
+
+        loginLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if ( loginIdEdit.getText().toString().length() == 0 || loginPwEdit.getText().toString().length() == 0) {
+                    Toast.makeText(LoginActivity.this,"아이디 또는 비밀번호를 입력해주세요.",Toast.LENGTH_SHORT).show();
+
+
+                } else {
+                    Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                    startActivity(intent);
+                }
+
+            }
+        });
+
     }
+
+
 }
