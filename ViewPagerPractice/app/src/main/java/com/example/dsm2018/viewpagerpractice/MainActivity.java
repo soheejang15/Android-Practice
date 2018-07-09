e551577f3aa7993a2ffc7980112d6ce9f557a8af
soheejang15 <jsh167528@gmail.com>
2018-07-09 09:10:23 +0900

    ViewPager-2

--- ViewPagerPractice/app/src/main/java/com/example/dsm2018/viewpagerpractice/MainActivity.java
+++ ViewPagerPractice/app/src/main/java/com/example/dsm2018/viewpagerpractice/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.dsm2018.viewpagerpractice;
 
 import android.graphics.Color;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,8 +19,8 @@ public class MainActivity extends AppCompatActivity {
         toolbar.setTitleTextColor(Color.parseColor("#ffffff"));
         setSupportActionBar(toolbar);
 
-    }
 
+    }
 
 
 }
