c7e9548c86ddc0919ccd49799ae2f977b5f4919b
soheejang15 <jsh167528@gmail.com>
2018-08-14 01:41:32 +0900

    Study layout_weight

---
+++ Do it Android/chap02/SampleLinearLayout/app/src/main/java/com/example/dsm2018/samplelinearlayout/WeightActivity.java
@@ -0,0 +1,13 @@
+package com.example.dsm2018.samplelinearlayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WeightActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weight);
+    }
+}
